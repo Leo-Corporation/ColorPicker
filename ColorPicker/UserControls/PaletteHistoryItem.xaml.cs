c5c9c5b25c2e85b0901d5fa24ebb45463b493dc9
Léo Peyronnet <leo.peyronnet@outlook.fr>
2021-10-19 16:36:43 +0200

    Added tooltips in history (#65)

--- ColorPicker/UserControls/PaletteHistoryItem.xaml.cs
+++ ColorPicker/UserControls/PaletteHistoryItem.xaml.cs
@@ -56,6 +56,7 @@ namespace ColorPicker.UserControls
 
 		private void InitUI()
 		{
+			// Borders
 			ColorB1.Background = new SolidColorBrush { Color = Color.FromRgb(Colors[0].R, Colors[0].G, Colors[0].B) }; // Set the background color
 			ColorB2.Background = new SolidColorBrush { Color = Color.FromRgb(Colors[1].R, Colors[1].G, Colors[1].B) }; // Set the background color
 			ColorB3.Background = new SolidColorBrush { Color = Color.FromRgb(Colors[2].R, Colors[2].G, Colors[2].B) }; // Set the background color
@@ -64,6 +65,16 @@ namespace ColorPicker.UserControls
 			ColorB6.Background = new SolidColorBrush { Color = Color.FromRgb(Colors[5].R, Colors[5].G, Colors[5].B) }; // Set the background color
 			ColorB7.Background = new SolidColorBrush { Color = Color.FromRgb(Colors[6].R, Colors[6].G, Colors[6].B) }; // Set the background color
 			ColorB8.Background = new SolidColorBrush { Color = Color.FromRgb(Colors[7].R, Colors[7].G, Colors[7].B) }; // Set the background color
+
+			// Tooltips
+			B1ToolTip.Content = $"{Properties.Resources.RGB}: {Colors[0].R}{Global.Settings.RGBSeparator}{Colors[0].G}{Global.Settings.RGBSeparator}{Colors[0].B}"; // Set tooltip text
+			B2ToolTip.Content = $"{Properties.Resources.RGB}: {Colors[1].R}{Global.Settings.RGBSeparator}{Colors[1].G}{Global.Settings.RGBSeparator}{Colors[1].B}"; // Set tooltip text
+			B3ToolTip.Content = $"{Properties.Resources.RGB}: {Colors[2].R}{Global.Settings.RGBSeparator}{Colors[2].G}{Global.Settings.RGBSeparator}{Colors[2].B}"; // Set tooltip text
+			B4ToolTip.Content = $"{Properties.Resources.RGB}: {Colors[3].R}{Global.Settings.RGBSeparator}{Colors[3].G}{Global.Settings.RGBSeparator}{Colors[3].B}"; // Set tooltip text
+			B5ToolTip.Content = $"{Properties.Resources.RGB}: {Colors[4].R}{Global.Settings.RGBSeparator}{Colors[4].G}{Global.Settings.RGBSeparator}{Colors[4].B}"; // Set tooltip text
+			B6ToolTip.Content = $"{Properties.Resources.RGB}: {Colors[5].R}{Global.Settings.RGBSeparator}{Colors[5].G}{Global.Settings.RGBSeparator}{Colors[5].B}"; // Set tooltip text
+			B7ToolTip.Content = $"{Properties.Resources.RGB}: {Colors[6].R}{Global.Settings.RGBSeparator}{Colors[6].G}{Global.Settings.RGBSeparator}{Colors[6].B}"; // Set tooltip text
+			B8ToolTip.Content = $"{Properties.Resources.RGB}: {Colors[7].R}{Global.Settings.RGBSeparator}{Colors[7].G}{Global.Settings.RGBSeparator}{Colors[7].B}"; // Set tooltip text
 		}
 
 		private void ColorB1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
