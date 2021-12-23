827dc7c7afcdd179634837f3f8e4dfcdaf93465c
Léo Peyronnet <leo.peyronnet@outlook.fr>
2021-12-23 10:46:26 +0100

    Fixed an issue with System theme not working on Windows 11

--- ColorPicker/Classes/Global.cs
+++ ColorPicker/Classes/Global.cs
@@ -162,7 +162,7 @@ namespace ColorPicker.Classes
 
 		public static bool IsSystemThemeDark()
 		{
-			if (Env.WindowsVersion != WindowsVersion.Windows10)
+			if (Env.WindowsVersion != WindowsVersion.Windows10 && Env.WindowsVersion != WindowsVersion.Windows11)
 			{
 				return false; // Avoid errors on older OSs
 			}
