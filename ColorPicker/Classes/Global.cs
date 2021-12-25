22b3bf01fcb4f4753de32832cdc27714b554487c
Léo Peyronnet <leo.peyronnet@outlook.fr>
2021-12-25 10:31:56 +0100

    Added Simplified Chinese language (#94)
    Thanks to @wcxu21 for the translations!
    
    Co-Authored-By: 大眼仔旭 <86864914+wcxu21@users.noreply.github.com>

--- ColorPicker/Classes/Global.cs
+++ ColorPicker/Classes/Global.cs
@@ -45,12 +45,12 @@ namespace ColorPicker.Classes
 		/// <summary>
 		/// List of the available languages.
 		/// </summary>
-		public static List<string> LanguageList => new() { "English (United States)", "Français (France)" };
+		public static List<string> LanguageList => new() { "English (United States)", "Français (France)", "中文（简体）" };
 
 		/// <summary>
 		/// List of the available languages codes.
 		/// </summary>
-		public static List<string> LanguageCodeList => new() { "en-US", "fr-FR" };
+		public static List<string> LanguageCodeList => new() { "en-US", "fr-FR", "zh-CN" };
 
 		/// <summary>
 		/// The <see cref="Pages.PickerPage"/>.
@@ -196,6 +196,10 @@ namespace ColorPicker.Classes
 
 				case "fr-FR": // French (FR)
 					Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("fr-FR"); // Change
+					break;
+
+				case "zh-CN": // Chinese (CN)
+					Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("zh-CN"); // Change
 					break;
 				default: // No language
 					break;
