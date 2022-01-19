aa487d9ca43ef5312c544b338563274c748dada7
Léo Peyronnet <leo.peyronnet@outlook.fr>
2022-01-19 10:05:24 +0100

    Code cleanup

--- ColorPicker/Windows/ColorWheelWindow.xaml.cs
+++ ColorPicker/Windows/ColorWheelWindow.xaml.cs
@@ -23,20 +23,10 @@ SOFTWARE.
 */
 using ColorPicker.Classes;
 using LeoCorpLibrary;
-using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace ColorPicker.Windows
