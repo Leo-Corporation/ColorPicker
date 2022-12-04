56635eb93c7885e5d6fd108d193233f6536fc513
Léo Peyronnet <leo.peyronnet@outlook.fr>
2022-12-04 14:02:28 +0100

    Version 4.6.0.2212

--- ColorPicker/Classes/Global.cs
+++ ColorPicker/Classes/Global.cs
@@ -44,7 +44,7 @@ public static class Global
 	/// <summary>
 	/// The current version of ColorPicker.
 	/// </summary>
-	public static string Version => "4.6.0.2212-rc1";
+	public static string Version => "4.6.0.2212";
 
 	/// <summary>
 	/// List of the available languages.
