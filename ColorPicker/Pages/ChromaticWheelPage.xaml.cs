5bc403e31ecfb604e1f20c480a572fc917cdf0d7
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-02-07 10:03:25 +0100

    Addedf the possibility to select different chromatic wheels (#202)

--- ColorPicker/Pages/ChromaticWheelPage.xaml.cs
+++ ColorPicker/Pages/ChromaticWheelPage.xaml.cs
@@ -1,3 +1,99 @@
+﻿/*
+MIT License
+
+Copyright (c) Léo Corporation
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. 
+*/
+using ColorPicker.Classes;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ColorPicker.Pages;
+/// <summary>
+/// Interaction logic for ChromaticWheelPage.xaml
+/// </summary>
+public partial class ChromaticWheelPage : Page
+{
+	public ChromaticWheelPage()
+	{
+		InitializeComponent();
+		InitUI();
+	}
+
+	private void InitUI()
+	{
+		TitleTxt.Text = $"{Properties.Resources.Picker} > {Properties.Resources.ChromaticWheel}";
+		CircleBtn_Click(this, null);
+	}
+
+	private void CircleBtn_Click(object sender, RoutedEventArgs e)
+	{
+		UnCheckAllButtons();
+		CheckButton(CircleBtn);
+
+		BitmapImage img = new();
+		img.BeginInit();
+		img.UriSource = new Uri("pack://application:,,,/ColorPicker;component/Images/ColorWheel.png");
+		img.EndInit();
+
+		WheelImg.Source = img; // Set the image source
+	}
+
+	private void DiscBtn_Click(object sender, RoutedEventArgs e)
+	{
+		UnCheckAllButtons();
+		CheckButton(DiscBtn);
+		BitmapImage img = new();
+		img.BeginInit();
+		img.UriSource = new Uri("pack://application:,,,/ColorPicker;component/Images/ColorDisc2.png");
+		img.EndInit();
+
+		WheelImg.Source = img; // Set the image source
+	}
+
+	private void ShadesBtn_Click(object sender, RoutedEventArgs e)
+	{
+		UnCheckAllButtons();
+		CheckButton(ShadesBtn);
+		BitmapImage img = new();
+		img.BeginInit();
+		img.UriSource = new Uri("pack://application:,,,/ColorPicker;component/Images/ColorDisc.png");
+		img.EndInit();
+
+		WheelImg.Source = img; // Set the image source
+	}
+
 	private void WheelImg_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
 	{
 		Bitmap bitmap = new(1, 1);
@@ -20,3 +116,52 @@
 		YuvTxt.Text = $"{colorInfo.YUV.Y:0.00}.., {colorInfo.YUV.U:0.00}.., {colorInfo.YUV.V:0.00}..";
 	}
 	ColorInfo ColorInfo { get; set; }
+	private void UnCheckAllButtons()
+	{
+		CircleBtn.Background = new SolidColorBrush { Color = Colors.Transparent };
+		DiscBtn.Background = new SolidColorBrush { Color = Colors.Transparent };
+		ShadesBtn.Background = new SolidColorBrush { Color = Colors.Transparent };
+	}
+
+	private void CheckButton(Button button) => button.Background = new SolidColorBrush { Color = Global.GetColorFromResource("LightAccentColor") };
+
+	private void CopyYiqBtn_Click(object sender, RoutedEventArgs e)
+	{
+		Clipboard.SetText($"{ColorInfo.YIQ.Y}, {ColorInfo.YIQ.I}, {ColorInfo.YIQ.Q}");
+	}
+
+	private void CopyXyzBtn_Click(object sender, RoutedEventArgs e)
+	{
+		Clipboard.SetText($"{ColorInfo.XYZ.X}, {ColorInfo.XYZ.Y}, {ColorInfo.XYZ.Z}");
+	}
+
+	private void CopyCmykBtn_Click(object sender, RoutedEventArgs e)
+	{
+		Clipboard.SetText($"{ColorInfo.CMYK.C}, {ColorInfo.CMYK.M}, {ColorInfo.CMYK.Y}, {ColorInfo.CMYK.K}");
+	}
+
+	private void CopyYuvBtn_Click(object sender, RoutedEventArgs e)
+	{
+		Clipboard.SetText($"{ColorInfo.YUV.Y}, {ColorInfo.YUV.U}, {ColorInfo.YUV.V}");
+	}
+
+	private void CopyHslBtn_Click(object sender, RoutedEventArgs e)
+	{
+		Clipboard.SetText(HslTxt.Text);
+	}
+
+	private void CopyHsvBtn_Click(object sender, RoutedEventArgs e)
+	{
+		Clipboard.SetText(HsvTxt.Text);
+	}
+
+	private void CopyHexBtn_Click(object sender, RoutedEventArgs e)
+	{
+		Clipboard.SetText(HexTxt.Text);
+	}
+
+	private void CopyRgbBtn_Click(object sender, RoutedEventArgs e)
+	{
+		Clipboard.SetText(RgbTxt.Text);
+	}
+}
