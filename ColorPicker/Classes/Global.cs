865f396f78e4d0efa4aa54bab15c9a5b381b544d
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-02-12 11:21:44 +0100

    Version 5.0.0.2302-pre4

--- ColorPicker/Classes/Global.cs
+++ ColorPicker/Classes/Global.cs
@@ -43,7 +43,7 @@ public static class Global
 
 	public static Bookmarks Bookmarks { get; set; }
 
-	public static string Version => "5.0.0.2302-pre3";
+	public static string Version => "5.0.0.2302-pre4";
 
 	public static string HiSentence
 	{
