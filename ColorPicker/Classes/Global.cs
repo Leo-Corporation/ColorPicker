12d8686f5175a213bc6739c09891ee315f18bd36
Léo Peyronnet <leo.peyronnet@outlook.fr>
2024-01-14 12:26:44 +0100

    Version 5.9.0.2401-rc1

--- ColorPicker/Classes/Global.cs
+++ ColorPicker/Classes/Global.cs
@@ -88,7 +88,7 @@ public static class Global
 	internal static string SettingsPath => $@"{FileSys.AppDataPath}\Léo Corporation\ColorPicker Max\Settings.xml";
 	public static string LastVersionLink => "https://raw.githubusercontent.com/Leo-Corporation/LeoCorp-Docs/master/Liens/Update%20System/ColorPicker/5.0/Version.txt";
 
-	public static string Version => "5.9.0.2401-rc1";
+	public static string Version => "5.9.0.2401";
 
 	public static string HiSentence
 	{
