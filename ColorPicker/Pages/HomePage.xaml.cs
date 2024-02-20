2c589afeabf09b43854526daf1c14ff22f1286f8
Léo Peyronnet <leo.peyronnet@outlook.fr>
2024-02-20 11:32:20 +0100

    Added palette quick action (#372)

--- ColorPicker/Pages/HomePage.xaml.cs
+++ ColorPicker/Pages/HomePage.xaml.cs
@@ -21,13 +21,18 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE. 
 */
-using ColorHelper;
 using ColorPicker.Classes;
+using ColorPicker.Enums;
 using ColorPicker.UserControls;
+using ColorPicker.Windows;
 using Synethia;
 using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Effects;
+using System.Windows.Shapes;
 
 namespace ColorPicker.Pages;
 /// <summary>
@@ -61,6 +66,7 @@ public partial class HomePage : Page
 			.Replace("LShiftKey", "Shift")
 			.Replace("RShiftKey", "Shift")
 			.Replace("RControlKey", "Ctrl"));
+		LoadPaletteUI();
 	}
 
 	private void SelectColor_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
@@ -84,14 +90,92 @@ public partial class HomePage : Page
 				_ => Global.GetColorFromResource("Red"),
 			};
 		}
-		catch
-		{
-
-		}
+		catch { }
 	}
 
-	private void Contrast_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+	private void LoadPaletteUI()
+	{
+		PalettePanel.Children.Clear();
+
+		(int r, int g, int b) = Global.GenerateRandomColor();
+		ColorInfo color = new(new((byte)r, (byte)g, (byte)b));
+
+		ColorBorder.Background = new SolidColorBrush { Color = Color.FromRgb((byte)r, (byte)g, (byte)b) };
+		ColorBorder.Effect = new DropShadowEffect() { BlurRadius = 15, ShadowDepth = 0, Color = Color.FromRgb((byte)r, (byte)g, (byte)b) };
+		ColorBorder.ToolTip = new ToolTip()
+		{
+			Background = Global.GetColorFromResource("Background1"),
+			Foreground = Global.GetColorFromResource("Foreground1"),
+			Content = color.ToString()
+		};
+
+		var hues = Global.GetHues(color.HSL);
+
+		for (int i = 0; i < hues.Length; i++)
+		{
+			CornerRadius radius = i == 0 ? new(10, 0, 0, 10) : new(0);
+			if (i == hues.Length - 1) radius = new(0, 10, 10, 0);
+
+			Border border = new()
+			{
+				Cursor = Cursors.Hand,
+				Height = 50,
+				Width = 50,
+				CornerRadius = radius,
+				Background = new SolidColorBrush { Color = Color.FromRgb(hues[i].R, hues[i].G, hues[i].B) },
+				Effect = new DropShadowEffect()
+				{
+					BlurRadius = 15,
+					Opacity = 0.2,
+					ShadowDepth = 0,
+					Color = Color.FromRgb(hues[i].R, hues[i].G, hues[i].B)
+				},
+				ToolTip = new ToolTip()
+				{
+					Background = Global.GetColorFromResource("Background1"),
+					Foreground = Global.GetColorFromResource("Foreground1"),
+					Content = new ColorInfo(new(hues[i].R, hues[i].G, hues[i].B)).ToString()
+				},
+			};
+			int j = i > hues.Length ? hues.Length - 1 : i; // Avoid index out of range
+			border.MouseLeftButtonUp += (o, e) =>
+			{
+				var info = new ColorInfo(new(hues[j].R, hues[j].G, hues[j].B));
+				Clipboard.SetText(Global.Settings.DefaultColorType switch
+				{
+					ColorTypes.HEX => info.HEX.Value,
+					ColorTypes.HSV => $"{info.HSV.H},{info.HSV.S},{info.HSV.V}",
+					ColorTypes.HSL => $"{info.HSL.H},{info.HSL.S},{info.HSL.L}",
+					ColorTypes.CMYK => $"{info.CMYK.C},{info.CMYK.M},{info.CMYK.Y},{info.CMYK.K}",
+					ColorTypes.XYZ => $"{info.XYZ.X}; {info.XYZ.Y}; {info.XYZ.Z}",
+					ColorTypes.YIQ => $"{info.YIQ.Y}; {info.YIQ.I}; {info.YIQ.Q}",
+					ColorTypes.YUV => $"{info.YUV.Y}; {info.YUV.U}; {info.YUV.V}",
+					ColorTypes.DEC => info.DEC.Value.ToString(),
+					_ => $"{hues[j].R}{Global.Settings.RgbSeparator}{hues[j].G}{Global.Settings.RgbSeparator}{hues[j].B}"
+				});
+			};
+
+			border.MouseRightButtonUp += (o, e) =>
+			{
+				new ColorDetailsWindow(new SolidColorBrush { Color = Color.FromRgb(hues[j].R, hues[j].G, hues[j].B) }).Show();
+				PalettePopup.IsOpen = false;
+			};
+
+			PalettePanel.Children.Add(border);
+		}
+	}
+	private void Contrast_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
 	{
 		ContrastPopup.IsOpen = true;
+	}
+
+	private void Palette_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+	{
+		PalettePopup.IsOpen = true;
+	}
+
+	private void ColorBorder_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+	{
+		LoadPaletteUI();
 	}
 }
