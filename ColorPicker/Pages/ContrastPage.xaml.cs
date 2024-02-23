363dbde8b222c965fe02031e7404ab12e04ffdef
Léo Peyronnet <leo.peyronnet@outlook.fr>
2024-02-23 11:05:14 +0100

    Added the possibility to input a color (#381)

--- ColorPicker/Pages/ContrastPage.xaml.cs
+++ ColorPicker/Pages/ContrastPage.xaml.cs
@@ -22,6 +22,10 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE. 
 */
 
+using ColorHelper;
+using ColorPicker.Classes;
+using ColorPicker.Enums;
+using Synethia;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -33,6 +37,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Effects;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -43,8 +48,322 @@ namespace ColorPicker.Pages;
 /// </summary>
 public partial class ContrastPage : Page
 {
+	bool code = !Global.Settings.UseSynethia; // checks if the code as already been implemented
 	public ContrastPage()
 	{
 		InitializeComponent();
+		InitUI();
+		Loaded += (o, e) => SynethiaManager.InjectSynethiaCode(this, Global.SynethiaConfig.PagesInfo, 9, ref code); // injects the code in the page
+	}
+
+	private void InitUI()
+	{
+		TitleTxt.Text = $"{Properties.Resources.ColorTools} > {Properties.Resources.ContrastGrid}";
+		try
+		{
+			(int r, int g, int b) = Global.GenerateRandomColor();
+			Txt1.Text = r.ToString();
+			Txt2.Text = g.ToString();
+			Txt3.Text = b.ToString();
+			ColorInfo = new(new((byte)r, (byte)g, (byte)b));
+		}
+		catch { }
+		RgbBtn_Click(Global.Settings.DefaultColorType switch
+		{
+			ColorTypes.HEX => HexBtn,
+			ColorTypes.HSV => HsvBtn,
+			ColorTypes.HSL => HslBtn,
+			ColorTypes.CMYK => CmykBtn,
+			ColorTypes.XYZ => XyzBtn,
+			ColorTypes.YIQ => YiqBtn,
+			ColorTypes.YUV => YuvBtn,
+			ColorTypes.DEC => DecBtn,
+			_ => RgbBtn
+		}, null);
+	}
+
+	internal Button SelectedColorBtn { get; set; }
+
+	private RGB ConvertToRgb()
+	{
+		if (SelectedColorBtn == RgbBtn) return new((byte)int.Parse(Txt1.Text),
+											 (byte)int.Parse(Txt2.Text),
+											 (byte)int.Parse(Txt3.Text));
+		if (SelectedColorBtn == HexBtn) return ColorHelper.ColorConverter.HexToRgb(new(Txt5.Text));
+		else if (SelectedColorBtn == HsvBtn) return ColorHelper.ColorConverter.HsvToRgb(new(int.Parse(Txt1.Text),
+											 (byte)int.Parse(Txt2.Text),
+											 (byte)int.Parse(Txt3.Text)));
+		else if (SelectedColorBtn == HslBtn) return ColorHelper.ColorConverter.HslToRgb(new(int.Parse(Txt1.Text),
+											 (byte)int.Parse(Txt2.Text),
+											 (byte)int.Parse(Txt3.Text)));
+		else if (SelectedColorBtn == CmykBtn) return ColorHelper.ColorConverter.CmykToRgb(new((byte)int.Parse(Txt1.Text),
+											 (byte)int.Parse(Txt2.Text),
+											 (byte)int.Parse(Txt3.Text),
+											 (byte)int.Parse(Txt4.Text)));
+		else if (SelectedColorBtn == XyzBtn) return ColorHelper.ColorConverter.XyzToRgb(new(double.Parse(Txt1.Text),
+											 double.Parse(Txt2.Text),
+											 double.Parse(Txt3.Text)));
+		else if (SelectedColorBtn == YuvBtn) return ColorHelper.ColorConverter.YuvToRgb(new(double.Parse(Txt1.Text),
+											 double.Parse(Txt2.Text),
+											 double.Parse(Txt3.Text)));
+		else if (SelectedColorBtn == DecBtn) return new DEC(int.Parse(Txt5.Text)).ToRgb();
+		else return ColorHelper.ColorConverter.YiqToRgb(new(double.Parse(Txt1.Text),
+											 double.Parse(Txt2.Text),
+											 double.Parse(Txt3.Text)));
+	}
+
+	private void UnCheckAllButtons()
+	{
+		RgbBtn.Background = new SolidColorBrush { Color = Colors.Transparent };
+		HexBtn.Background = new SolidColorBrush { Color = Colors.Transparent };
+		HsvBtn.Background = new SolidColorBrush { Color = Colors.Transparent };
+		HslBtn.Background = new SolidColorBrush { Color = Colors.Transparent };
+		CmykBtn.Background = new SolidColorBrush { Color = Colors.Transparent };
+		XyzBtn.Background = new SolidColorBrush { Color = Colors.Transparent };
+		YiqBtn.Background = new SolidColorBrush { Color = Colors.Transparent };
+		YuvBtn.Background = new SolidColorBrush { Color = Colors.Transparent };
+		DecBtn.Background = new SolidColorBrush { Color = Colors.Transparent };
+	}
+
+	internal void RgbBtn_Click(object sender, RoutedEventArgs? e)
+	{
+		var btn = (Button)sender;
+
+		UnCheckAllButtons();
+		CheckButton(btn);
+		SelectedColorBtn = btn;
+		LoadInputUI();
+		Global.SynethiaConfig.PagesInfo[9].InteractionCount++;
+	}
+
+	internal void CheckButton(Button button) => button.Background = Global.GetColorFromResource("LightAccentColor");
+
+	private void HideAllInput()
+	{
+		DisplayText1.Visibility = Visibility.Collapsed;
+		DisplayText2.Visibility = Visibility.Collapsed;
+		DisplayText3.Visibility = Visibility.Collapsed;
+		DisplayText4.Visibility = Visibility.Collapsed;
+		DisplayText5.Visibility = Visibility.Collapsed; // Special textbox for hex
+
+		// Clear text to avoid errors
+		Txt1.Text = "";
+		Txt2.Text = "";
+		Txt3.Text = "";
+		Txt4.Text = "";
+		Txt5.Text = ""; // Special textbox for hex
+
+		B1.Visibility = Visibility.Collapsed;
+		B2.Visibility = Visibility.Collapsed;
+		B3.Visibility = Visibility.Collapsed;
+		B4.Visibility = Visibility.Collapsed;
+		B5.Visibility = Visibility.Collapsed; // Special textbox for hex
+	}
+	internal ColorInfo ColorInfo { get; set; }
+	private void LoadInputUI()
+	{
+		HideAllInput();
+		if (SelectedColorBtn != HexBtn && SelectedColorBtn != DecBtn)
+		{
+			DisplayText1.Visibility = Visibility.Visible;
+			DisplayText2.Visibility = Visibility.Visible;
+			DisplayText3.Visibility = Visibility.Visible;
+			DisplayText4.Visibility = SelectedColorBtn == CmykBtn ? Visibility.Visible : Visibility.Collapsed;
+
+			B1.Visibility = Visibility.Visible;
+			B2.Visibility = Visibility.Visible;
+			B3.Visibility = Visibility.Visible;
+			B4.Visibility = SelectedColorBtn == CmykBtn ? Visibility.Visible : Visibility.Collapsed;
+		}
+
+		if (SelectedColorBtn == RgbBtn)
+		{
+			DisplayText1.Text = "R";
+			DisplayText2.Text = "G";
+			DisplayText3.Text = "B";
+
+			Txt1.Text = ColorInfo.RGB.R.ToString();
+			Txt2.Text = ColorInfo.RGB.G.ToString();
+			Txt3.Text = ColorInfo.RGB.B.ToString();
+		}
+		else if (SelectedColorBtn == HexBtn)
+		{
+			DisplayText5.Visibility = Visibility.Visible;
+
+			DisplayText5.Text = Properties.Resources.HEX;
+			B5.Visibility = Visibility.Visible;
+
+			Txt5.Text = ColorInfo.HEX.Value;
+		}
+		else if (SelectedColorBtn == HsvBtn)
+		{
+			DisplayText1.Text = "H";
+			DisplayText2.Text = "S";
+			DisplayText3.Text = "V";
+
+			Txt1.Text = ColorInfo.HSV.H.ToString();
+			Txt2.Text = ColorInfo.HSV.S.ToString();
+			Txt3.Text = ColorInfo.HSV.V.ToString();
+		}
+		else if (SelectedColorBtn == HslBtn)
+		{
+			DisplayText1.Text = "H";
+			DisplayText2.Text = "S";
+			DisplayText3.Text = "L";
+
+			Txt1.Text = ColorInfo.HSL.H.ToString();
+			Txt2.Text = ColorInfo.HSL.S.ToString();
+			Txt3.Text = ColorInfo.HSL.L.ToString();
+		}
+		else if (SelectedColorBtn == CmykBtn)
+		{
+			DisplayText1.Text = "C";
+			DisplayText2.Text = "M";
+			DisplayText3.Text = "Y";
+			DisplayText4.Text = "K";
+
+			Txt1.Text = ColorInfo.CMYK.C.ToString();
+			Txt2.Text = ColorInfo.CMYK.M.ToString();
+			Txt3.Text = ColorInfo.CMYK.Y.ToString();
+			Txt4.Text = ColorInfo.CMYK.K.ToString();
+		}
+		else if (SelectedColorBtn == XyzBtn)
+		{
+			DisplayText1.Text = "X";
+			DisplayText2.Text = "Y";
+			DisplayText3.Text = "Z";
+
+			Txt1.Text = ColorInfo.XYZ.X.ToString();
+			Txt2.Text = ColorInfo.XYZ.Y.ToString();
+			Txt3.Text = ColorInfo.XYZ.Z.ToString();
+		}
+		else if (SelectedColorBtn == YiqBtn)
+		{
+			DisplayText1.Text = "Y";
+			DisplayText2.Text = "I";
+			DisplayText3.Text = "Q";
+
+			Txt1.Text = ColorInfo.YIQ.Y.ToString();
+			Txt2.Text = ColorInfo.YIQ.I.ToString();
+			Txt3.Text = ColorInfo.YIQ.Q.ToString();
+		}
+		else if (SelectedColorBtn == YuvBtn)
+		{
+			DisplayText1.Text = "Y";
+			DisplayText2.Text = "U";
+			DisplayText3.Text = "V";
+
+			Txt1.Text = ColorInfo.YUV.Y.ToString();
+			Txt2.Text = ColorInfo.YUV.U.ToString();
+			Txt3.Text = ColorInfo.YUV.V.ToString();
+		}
+		else if (SelectedColorBtn == DecBtn)
+		{
+			DisplayText5.Visibility = Visibility.Visible;
+
+			DisplayText5.Text = Properties.Resources.DEC;
+			B5.Visibility = Visibility.Visible;
+
+			Txt5.Text = ColorInfo.DEC.Value.ToString();
+		}
+	}
+
+	private void Txt1_TextChanged(object sender, TextChangedEventArgs e)
+	{
+		try
+		{
+			InitGrid();
+		}
+		catch { }
+	}
+
+	private void Txt1_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+	{
+		if (e.Command == ApplicationCommands.Paste)
+		{
+			e.CanExecute = true;
+			e.Handled = true;
+		}
+	}
+
+	private void Txt1_PreviewExecuted(object sender, ExecutedRoutedEventArgs e)
+	{
+		try
+		{
+			if (e.Command == ApplicationCommands.Paste)
+			{
+				string text = Clipboard.GetText()
+					.Replace("(", "")
+					.Replace(")", "")
+					.Replace(" ", "");
+				if (SelectedColorBtn == HsvBtn || SelectedColorBtn == HslBtn || SelectedColorBtn == CmykBtn)
+				{
+					var split = text.Split(",");
+					Txt1.Text = split[0];
+					Txt2.Text = split[1];
+					Txt3.Text = split[2];
+					Txt4.Text = split.Length > 3 ? split[3] : "";
+				}
+				else if (SelectedColorBtn == HexBtn || SelectedColorBtn == DecBtn)
+				{
+					Txt5.Text = text;
+				}
+				else
+				{
+					var split = text.Split(new string[] { ";", Global.Settings.RgbSeparator ?? ";" }, StringSplitOptions.None);
+					Txt1.Text = split[0];
+					Txt2.Text = split[1];
+					Txt3.Text = split[2];
+				}
+
+				e.Handled = true;
+			}
+		}
+		catch { }
+	}
+
+	internal void ColorBorder_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+	{
+		try
+		{
+			(int r, int g, int b) = Global.GenerateRandomColor();
+			ColorInfo = new(new((byte)r, (byte)g, (byte)b));
+			Global.SynethiaConfig.ActionsInfo[9].UsageCount++; // Increment the usage counter
+		}
+		catch { }
+		RgbBtn_Click(SelectedColorBtn, null);
+	}
+
+	private void BookmarkBtn_Click(object sender, RoutedEventArgs e)
+	{
+		if (Global.Bookmarks.ColorBookmarks.Contains($"#{ColorInfo.HEX.Value}"))
+		{
+			Global.Bookmarks.ColorBookmarks.Remove($"#{ColorInfo.HEX.Value}");
+			BookmarkBtn.Content = "\uF1F6";
+			BookmarkToolTip.Content = Properties.Resources.AddBookmark;
+
+			return;
+		}
+		Global.Bookmarks.ColorBookmarks.Add($"#{ColorInfo.HEX.Value}"); // Add to color bookmarks
+		BookmarkBtn.Content = "\uF1F8";
+		BookmarkToolTip.Content = Properties.Resources.RemoveBookmark;
+	}
+
+	internal void InitGrid()
+	{
+		ColorInfo = new ColorInfo(ConvertToRgb());
+		var color = Color.FromRgb(ColorInfo.RGB.R, ColorInfo.RGB.G, ColorInfo.RGB.B);
+		ColorBorder.Background = new SolidColorBrush { Color = color };
+		ColorBorder.Effect = new DropShadowEffect() { BlurRadius = 15, ShadowDepth = 0, Color = color };
+
+		// Load the bookmark icon
+		if (!Global.Bookmarks.ColorBookmarks.Contains($"#{ColorInfo.HEX.Value}"))
+		{
+			BookmarkBtn.Content = "\uF1F6";
+			BookmarkToolTip.Content = Properties.Resources.AddBookmark;
+			return;
+		}
+		BookmarkBtn.Content = "\uF1F8";
+		BookmarkToolTip.Content = Properties.Resources.RemoveBookmark;
 	}
 }
