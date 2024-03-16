0c0f13f938fcaa629d32ad37cf1703158bf69dd7
Léo Peyronnet <leo.peyronnet@outlook.fr>
2024-03-16 12:33:54 +0100

    Added translations (#395)

--- ColorPicker/Properties/Resources.Designer.cs
+++ ColorPicker/Properties/Resources.Designer.cs
@@ -70,6 +70,15 @@ namespace ColorPicker.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Add a note.
+        /// </summary>
+        public static string AddNote {
+            get {
+                return ResourceManager.GetString("AddNote", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to I agree.
         /// </summary>
         public static string Agree {
@@ -1470,6 +1479,15 @@ namespace ColorPicker.Properties {
         public static string Rotation {
             get {
                 return ResourceManager.GetString("Rotation", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Save.
+        /// </summary>
+        public static string Save {
+            get {
+                return ResourceManager.GetString("Save", resourceCulture);
             }
         }
         
