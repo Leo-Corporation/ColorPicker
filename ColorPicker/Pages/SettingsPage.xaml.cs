4d41346f17700978fe15fd3a818aa2d101e12ae5
Léo Peyronnet <leo.peyronnet@outlook.fr>
2026-02-18 13:10:36 +0100

    chore: Update Licenses (#477)

--- ColorPicker/Pages/SettingsPage.xaml.cs
+++ ColorPicker/Pages/SettingsPage.xaml.cs
@@ -522,8 +522,8 @@ public partial class SettingsPage : Page
 		"Fluent System Icons - MIT License - © 2020 Microsoft Corporation\n" +
 		"ColorHelper - MIT License - © 2020 Artyom Gritsuk\n" +
 		"globalmousekeyhook - MIT License - © 2010-2018 George Mamaladze\n" +
-		"PeyrSharp - MIT License - © 2022-2025 Devyus\n" +
-		"ColorPicker - MIT License - © 2021-2025 Léo Corporation", $"{Properties.Resources.ColorPickerMax} - {Properties.Resources.Licenses}", MessageBoxButton.OK, MessageBoxImage.Information);
+		"PeyrSharp - MIT License - © 2022-2026 Léo Corporation\n" +
+		"ColorPicker - MIT License - © 2021-2026 Léo Corporation", $"{Properties.Resources.ColorPickerMax} - {Properties.Resources.Licenses}", MessageBoxButton.OK, MessageBoxImage.Information);
 	}
 
 	private void ApiApplyBtn_Click(object sender, RoutedEventArgs e)
